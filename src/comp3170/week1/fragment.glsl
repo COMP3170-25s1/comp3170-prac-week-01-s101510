0789cbed924e4012109bd737d6ba5bac8584c5a9
s101510 <s101510yingwa@gmail.com>
2025-02-27 16:58:28 +1100

    prac week 1
    up to changing line 10 step

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,10 +7,10 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
-  
-   if (d < 0.5) {
-      o_colour = vec4(u_colour, 1);
+   vec2 v = abs(p - vec2(0.5, 0.5));
+   float d = max(v.x, v.y);    
+   if (d < 0.2) {
+      o_colour = vec4(u_colour, 0);
    }
    else {
       o_colour = vec4(0,0,0,1); // BLACK
